19982d5c37fd530663fc3a85a4015b56fa7affa1
Richard-Alves167 <riichardd.08@gmail.com>
2026-01-13 11:09:41 -0300

    FEAT: adiciona a classe Compra

---
+++ Mercado-Web-API/Models/Compra.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mercado_Web_API.Models {
+    public class Compra {
+        public Compra(int idCliente) {
+            IdCliente = idCliente;
+            Data = DateTime.Now;
+        }
+        [Required]
+        public int IdCliente { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
