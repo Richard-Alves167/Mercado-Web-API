d7108810f90da8f948f551a68d741d88239e3304
Richard-Alves167 <riichardd.08@gmail.com>
2026-01-15 11:57:05 -0300

    FEAT: adiciona a camada de Serviço com Interface para Cliente

--- Mercado-Web-API/obj/Debug/net8.0/Mercado-Web-API.AssemblyInfo.cs
+++ Mercado-Web-API/obj/Debug/net8.0/Mercado-Web-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mercado-Web-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6100929b54b6b5c1abc3552912fcb0dba0f00f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586a2e2e389d58767dd798462f4a2cef9006fcb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mercado-Web-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mercado-Web-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
