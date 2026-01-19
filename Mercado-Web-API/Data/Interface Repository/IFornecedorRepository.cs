2f495eacb49c0c0380c55912b1f6a0f526b26fbd
Richard-Alves167 <riichardd.08@gmail.com>
2026-01-19 16:39:36 -0300

    FIX: refaz a interface de repositorio para Fornecedor com a relação da classe de FornecedorProduto

--- Mercado-Web-API/Data/Interface Repository/IFornecedorRepository.cs
+++ Mercado-Web-API/Data/Interface Repository/IFornecedorRepository.cs
@@ -1,13 +1,15 @@
 ﻿using Mercado_Web_API.Models;
+using Mercado_Web_API.Models.Relationships;
 
 namespace Mercado_Web_API.Data.Interface_Repository {
     public interface IFornecedorRepository {
         public void Add(Fornecedor produto);
         public List<Fornecedor> GetAll();
         public Fornecedor GetById(int id);
-        public List<Produto> GetAllProductsByFornecedorId(int id);
-        public void AddFornecedorToProduto(int idFornecedor, int idProduto);
-        public void RemoveFornecedorOfProduto(int idFornecedor, int idProduto);
+        public List<Produto> GetAllProductsByFornecedorId(int idFornecedor);
+        public void AddFornecedorProduto(FornecedorProduto fornecedorProduto);
+        public void GetFornecedorProduto(int idFornecedor, int idProduto);
+        public void RemoveFornecedorProduto(FornecedorProduto fornecedorProduto);
         public void Delete(Fornecedor Fornecedor);
     }
 }
