cb94222781b45bb7d8a410736d2b76f984cd14cb
Richard-Alves167 <riichardd.08@gmail.com>
2026-01-22 22:27:43 -0300

    FEAT: adiciona a interface e a implementação do repositório com Entity Framework de Item

---
+++ Mercado-Web-API/Data/Interface Repository/IItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using Mercado_Web_API.Models;
+
+namespace Mercado_Web_API.Data.Interface_Repository {
+    public interface IItemRepository {
+        public void Add(Item item);
+        public List<Item> GetAll();
+        public Item GetById(long id);
+        public void Delete(Item item);
+    }
+}
---
+++ Mercado-Web-API/Data/RepositoryEF/ItemRepositoryEF.cs
@@ -0,0 +1,30 @@
+﻿using Mercado_Web_API.Data.Interface_Repository;
+using Mercado_Web_API.Models;
+
+namespace Mercado_Web_API.Data.RepositoryEF {
+    public class ItemRepositoryEF : IItemRepository {
+        private MercadoContext _context;
+        public ItemRepositoryEF(MercadoContext context) {
+            _context = context;
+        }
+
+        public void Add(Item item) {
+            _context.Itens.Add(item);
+            _context.SaveChanges();
+        }
+
+        public List<Item> GetAll() {
+            List<Item> itens = _context.Itens.ToList();
+            return itens;
+        }
+
+        public Item GetById(long id) {
+            Item item = _context.Itens.FirstOrDefault(i => i.Id == id);
+            return item;
+        }
+        public void Delete(Item item) {
+            _context.Itens.Remove(item);
+            _context.SaveChanges();
+        }
+    }
+}
