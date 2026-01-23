c086b0bfef1d17e83fefd86afc1c89c4d4e47cb6
Richard-Alves167 <riichardd.08@gmail.com>
2026-01-22 22:37:01 -0300

    FEAT: adiciona DTOs de criação e leitura para Item

--- Mercado-Web-API/ModelDTOs/ItemCreateDTO.cs
+++ Mercado-Web-API/ModelDTOs/ItemCreateDTO.cs
@@ -1,21 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Numerics;
 
-namespace Mercado_Web_API.Models {
-    public class Item {
-        public Item(long compraId, int produtoId, decimal preco, int quantidade) {
-            CompraId = compraId;
-            ProdutoId = produtoId;
-            Preco = preco;
-            Quantidade = quantidade;
-        }
-        public long Id { get; set; }
+namespace Mercado_Web_API.ModelDTOs {
+    public class ItemCreateDTO {
         [Required]
         public long CompraId { get; set; }
-        public Compra Compra { get; set; }
         [Required]
         public int ProdutoId { get; set; }
-        public Produto Produto { get; set; }
         [Required]
         [Range(0.01, 9999, ErrorMessage = "O preço deve ser maior que zero")]
         public decimal Preco { get; set; }
---
+++ Mercado-Web-API/ModelDTOs/ItemReadDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mercado_Web_API.ModelDTOs {
+    public class ItemReadDTO {
+        public long Id { get; set; }
+        public long CompraId { get; set; }
+        public int ProdutoId { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
